d96cbb0a929e72d452b85b69ecd7cad83dc5e357
intbit <syed.muzamil@intbittech.com>
2015-06-24 14:22:03 +0530

    Added a jar file

--- BrndBot/src/java/com/controller/sendEmail.java
+++ BrndBot/src/java/com/controller/sendEmail.java
@@ -22,7 +22,6 @@ import java.io.InputStream;
 import java.io.StringWriter;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
