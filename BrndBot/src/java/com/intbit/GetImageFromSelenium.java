4a3debea55065efcaaa6200c29301291a0de08b6
AbdulRaqeeb AbdulAzeez <ar@intbittech.com>
2015-07-24 19:34:34 -0400

    Adding template and convert from html string to html file

--- BrndBot/src/java/com/intbit/GetImageFromSelenium.java
+++ BrndBot/src/java/com/intbit/GetImageFromSelenium.java
@@ -6,8 +6,12 @@
 package com.intbit;
 
 import java.io.File;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
@@ -21,7 +25,11 @@ import org.openqa.selenium.remote.RemoteWebDriver;
  */
 public class GetImageFromSelenium {
 
-    public static File getImage(String url) throws MalformedURLException {
+    private static final String kEncoding = "UTF-8";
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMddHH:mm:ssSSS");
+
+    public static File getImage(String htmlString) throws MalformedURLException, IOException {
+
         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
         desiredCapabilities.setJavascriptEnabled(true);
         desiredCapabilities.setCapability("takesScreenshot", true);
@@ -29,12 +37,27 @@ public class GetImageFromSelenium {
         WebDriver driver = new RemoteWebDriver(new URL(
                 "http://127.0.0.1:4444/wd/hub"), desiredCapabilities);
 
-        driver.navigate().to(url);
+        File createdHtmlFile = tempHTML(htmlString);
+        driver.navigate().to(createdHtmlFile.getPath());
 
         driver = new Augmenter().augment(driver);
         File srcFile = ((TakesScreenshot) driver)
                 .getScreenshotAs(OutputType.FILE);
         driver.quit();
+        //Deleting file the using it
+        createdHtmlFile.delete();
         return srcFile;
+    }
+
+    public static File tempHTML(String bodyString) throws IOException {
+
+        File htmlTemplateFile = new File("admin/template.html");
+        String htmlString = FileUtils.readFileToString(htmlTemplateFile, kEncoding);
+        htmlString = htmlString.replace("$body", bodyString);
+        String fileName = dateFormat.format(new Date());
+        //Using the date as the unique file name
+        File newHtmlFile = new File("temp/"+fileName+".html");
+        FileUtils.writeStringToFile(newHtmlFile, htmlString, kEncoding);
+        return newHtmlFile;
     }
 }
