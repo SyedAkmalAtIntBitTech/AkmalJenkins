7d6af42125f643819607a8bc9b7ed735ab5d678d
AbdulRaqeeb AbdulAzeez <ar@intbittech.com>
2015-08-20 02:51:31 -0400

    Added HTML template file path

--- BrndBot/src/java/com/intbit/AppConstants.java
+++ BrndBot/src/java/com/intbit/AppConstants.java
@@ -20,7 +20,9 @@ public class AppConstants {
     public static final String BASE_FONT_UPLOAD_PATH = BASE_UPLOAD_PATH + File.separator + "fonts";
     
     public static final String BASE_XML_UPLOAD_PATH = BASE_UPLOAD_PATH + File.separator + "xml";
-   
+
+    public static final String BASE_HTML_TEMPLATE_UPLOAD_PATH = BASE_UPLOAD_PATH + File.separator + "HTMLTemplates";
+
     public static final String BASE_MODEL_PATH = BASE_UPLOAD_PATH + File.separator + "model";
             
     public static final String USER_IMAGE_HOME = BASE_IMAGE_UPLOAD_PATH + File.separator + "Gallery";
