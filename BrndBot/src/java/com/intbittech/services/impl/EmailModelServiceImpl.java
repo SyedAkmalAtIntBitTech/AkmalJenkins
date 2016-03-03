4c6244a618330d15df25b700e674ac647b04007a
ajitpandeyatintbittech <ajit.pandey@intbittech.com>
2016-03-03 19:42:57 +0530

    added emailmodeldao

---
+++ BrndBot/src/java/com/intbittech/services/impl/EmailModelServiceImpl.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright 2015 Intbit Technologies. This software and documentation contains
+ * confidential and proprietary information that is owned by Intbit
+ * Technologies. Unauthorized use and distribution are strictly prohibited.
+ */
+package com.intbittech.services.impl;
+
+/**
+ *
+ * @author ajit
+ */
+public class EmailModelServiceImpl {
+    
+}
