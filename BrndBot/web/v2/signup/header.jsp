3eaacb608436fb590272fe6826d929c63ef586f9
SyedAkmalAtIntBitTech <syed.akmal@intbittech.com>
2016-04-07 21:13:15 +0530

    colors from theme implemented

--- BrndBot/web/v2/signup/header.jsp
+++ BrndBot/web/v2/signup/header.jsp
@@ -30,3 +30,4 @@
 <script src="js/configurations.js"></script>   
 <link href="css/colpick.css" rel="stylesheet" type="text/css"/>
 <script src="js/colpick.js" type="text/javascript"></script>
+<link rel="shortcut icon" href="images/favicon.png"/> 
--- BrndBot/web/v2/signup/js/onboarding.js
+++ BrndBot/web/v2/signup/js/onboarding.js
@@ -3,16 +3,31 @@
  * confidential and proprietary information that is owned by Intbit
  * Technologies. Unauthorized use and distribution are strictly prohibited.
  */
+    function getColorID(idNo,color){
+        $('.palette-colorswab-selected' ).css("background-color",color);
+    }
+    
+    function getThemeColors(c1,c2,c3,c4){
+        $("#color1").css("background-color",c1);
+        $("#color2").css("background-color",c2);
+        $("#color3").css("background-color",c3);
+        $("#color4").css("background-color",c4);
+        $(".selected-palette-colorswab").removeClass('selected-palette-colorswab').addClass('select-palette-colorswab');
+        $(".palette-colorswab-selected").removeClass('palette-colorswab-selected').addClass('palette-colorswab');
+    }
+    
 $(document).ready(function (){
-   
+    
     $("#logoColorsDiv").click(function (){
         $("#picker").hide();
         $("#logocolor").show();
+        $("#themeColorsDiv").hide();
         $("#themeColors,#customColors").removeClass('colorOptions-tab-active').addClass('colorOptions-tab');
         $("#logoColorsDiv").removeClass('colorOptions-tab').addClass('colorOptions-tab-active');
     });
     
     $("#themeColors").click(function (){
+        $("#themeColorsDiv").show();
         $("#picker").hide();
         $("#logocolor").hide();
         $("#logoColorsDiv,#customColors").removeClass('colorOptions-tab-active').addClass('colorOptions-tab');
@@ -20,21 +35,38 @@ $(document).ready(function (){
     });
     
     $("#customColors").click(function (){
+        $("#themeColorsDiv").hide();
         $("#picker").show();
         $("#logocolor").hide();
         $("#themeColors,#logoColorsDiv").removeClass('colorOptions-tab-active').addClass('colorOptions-tab');
         $("#customColors").removeClass('colorOptions-tab').addClass('colorOptions-tab-active');
     });
     
-     $('#picker').colpick({
-        flat:true,
-                layout:'hex',
-                onSubmit:function(hsb, hex, rgb, el) {
-                //for haking hex value alert(hex);
-
-                $('#' + ElementID).css("background-color", "#" + hex);
-                }
+   var globalColorPaletteDivId="";
+    $(".palette-colorswab , .palette-colorswab-selected").click(function (){
+        
+//        $(".palette-colorswab").removeClass('palette-colorswab-selected');
+//        $(".palette-colorswab").addClass('palette-coloswab');
+        var colorPaletteDivId=$(this).attr('id');
+        globalColorPaletteDivId=colorPaletteDivId;   
+        $(this).toggleClass('palette-colorswab-selected').toggleClass('palette-colorswab');    
     });
+    
+    $(".select-palette-colorswab").click(function (){
+        $(this).toggleClass('selected-palette-colorswab').toggleClass('select-palette-colorswab');            
+    });
+    
+     $('#picker').colpick({
+        flat: true,
+        layout: 'hex',
+        onSubmit: function (hsb, hex, rgb, el) {
+            //for haking hex value alert(hex);
+            $('.palette-colorswab-selected' ).css("background-color", "#" + hex);
+        }
+    });
+    
+    
+    
     
 });
 
@@ -200,7 +232,7 @@ function onboardingcontroller($scope,$http) {
     $scope.getLogoColors = function () {
         $http({
             method: 'GET',
-            url: '../GetColorsFromLogo'
+            url: getHost() +'/GetColorsFromLogo'
         }).success(function (data, status, headers, config) {
             $scope.color = data.Colors;
             if (data === error) {
@@ -210,7 +242,27 @@ function onboardingcontroller($scope,$http) {
             alert("No data available! Problem fetching the data.");
         });
     };
-            
+    
+     $scope.showThemeColors = function( ){
+             $scope.curPage = 0;
+             $scope.pageSize = 10;
+                $http({
+                        method : 'GET',
+                        url : getHost() +'/getAllColorThemes.do'
+                }).success(function(data, status, headers, config) {
+                    $scope.themeColors = data.d.details;
+                }).error(function(data, status, headers, config) {
+                    alert(eval(JSON.stringify(data.d.operationStatus.messages)));
+                });
+    };
+    
+    $scope.clearColorPalette = function (){
+        var bgColor="background-color";
+        $("#color1").css(bgColor,"");
+        $("#color2").css(bgColor,"");
+        $("#color3").css(bgColor,"");
+        $("#color4").css(bgColor,"");
+    };
     
 };
 
