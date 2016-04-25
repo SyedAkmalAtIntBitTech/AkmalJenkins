135b5ac76e0572f5870ca165d6fdacce8cbab232
AbdulRaqeeb AbdulAzeez <ar@intbittech.com>
2016-04-24 23:51:45 -0400

    Set Email List Servelet and get

--- BrndBot/src/java/com/intbittech/controller/DownloadController.java
+++ BrndBot/src/java/com/intbittech/controller/DownloadController.java
@@ -5,40 +5,22 @@
  */
 package com.intbittech.controller;
 
-import com.controller.ApplicationContextListener;
-import com.controller.GenerateHashPassword;
 import com.google.gson.Gson;
 import com.intbit.AppConstants;
-import com.intbittech.model.ForgotPassword;
-import com.intbittech.model.UserProfile;
-import com.intbittech.responsemappers.ContainerResponse;
-import com.intbittech.responsemappers.TransactionResponse;
-import com.intbittech.services.ForgotPasswordService;
-import com.intbittech.utility.ErrorHandlingUtil;
-import com.intbittech.utility.UserSessionUtil;
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.util.HashMap;
-import java.util.Locale;
 import java.util.Map;
-import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.apache.log4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.MessageSource;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import util.Utility;
 
 /**
  *
--- BrndBot/src/java/com/intbittech/controller/EmailListController.java
+++ BrndBot/src/java/com/intbittech/controller/EmailListController.java
@@ -5,20 +5,16 @@
  */
 package com.intbittech.controller;
 
-import com.controller.ApplicationContextListener;
-import com.controller.GenerateHashPassword;
 import com.intbit.AppConstants;
-import com.intbittech.model.ForgotPassword;
 import com.intbittech.model.UserProfile;
 import com.intbittech.responsemappers.ContainerResponse;
 import com.intbittech.responsemappers.TransactionResponse;
-import com.intbittech.services.ForgotPasswordService;
+import com.intbittech.services.EmailListService;
 import com.intbittech.utility.ErrorHandlingUtil;
 import com.intbittech.utility.UserSessionUtil;
 import java.io.BufferedReader;
 import java.util.Locale;
 import java.util.Map;
-import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.apache.log4j.Logger;
@@ -28,11 +24,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import util.Utility;
 
 /**
  *
@@ -42,35 +35,24 @@ import util.Utility;
 @RequestMapping(value = "/emaillist")
 public class EmailListController {
 
-    private Logger logger = Logger.getLogger(EmailListController.class);
+    private final Logger logger = Logger.getLogger(EmailListController.class);
 
     @Autowired
-    ForgotPasswordService forgotPasswordService;
+    EmailListService emailListService;
     @Autowired
     private MessageSource messageSource;
 
-    @RequestMapping(value = "/{jspFileName}", method = RequestMethod.GET)
-    public String signUpJspPages(ModelMap model, @PathVariable(value = "jspFileName") String jspFileName) {
-        return "signup/" + jspFileName;
-    }
-
-    @RequestMapping(value = "/forgotPassword", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<ContainerResponse> forgotSendEMail(HttpServletRequest request,
+    @RequestMapping(value = "/get", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<ContainerResponse> getEmailList(HttpServletRequest request,
             HttpServletResponse response) {
         TransactionResponse transactionResponse = new TransactionResponse();
         try {
+            UserProfile userProfile = (UserProfile) UserSessionUtil.getLogedInUser();
+            Integer userId = userProfile.getUser().getUserId();
             Map<String, Object> requestBodyMap
                     = AppConstants.GSON.fromJson(new BufferedReader(request.getReader()), Map.class);
-
-            ServletContext servletContext = ApplicationContextListener.getApplicationServletContext();
-            String context_real_path = servletContext.getRealPath("");
-
-            String contextPath = Utility.getServerName(context_real_path);
-
-            String email_id = (String) requestBodyMap.get("emailid");
-
-            forgotPasswordService.sendMail(email_id, contextPath);
-            transactionResponse.setOperationStatus(ErrorHandlingUtil.dataNoErrorValidation(messageSource.getMessage("signup_pleasecheckmail", new String[]{}, Locale.US)));
+            transactionResponse.setMessage(emailListService.getEmailList(requestBodyMap, userId));
+            transactionResponse.setOperationStatus(ErrorHandlingUtil.dataNoErrorValidation(messageSource.getMessage("success", new String[]{}, Locale.US)));
 
         } catch (Throwable throwable) {
             logger.error(throwable);
@@ -79,36 +61,28 @@ public class EmailListController {
         return new ResponseEntity<>(new ContainerResponse(transactionResponse), HttpStatus.ACCEPTED);
     }
 
-    @RequestMapping(value = "/resetPassword", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<ContainerResponse> resetPassword(HttpServletRequest request,
+    @RequestMapping(value = "/save", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<ContainerResponse> setEmailList(HttpServletRequest request,
             HttpServletResponse response) {
         TransactionResponse transactionResponse = new TransactionResponse();
         try {
+            UserProfile userProfile = (UserProfile) UserSessionUtil.getLogedInUser();
+            Integer userId = userProfile.getUser().getUserId();
             Map<String, Object> requestBodyMap
                     = AppConstants.GSON.fromJson(new BufferedReader(request.getReader()), Map.class);
-
-            String type = (String) requestBodyMap.get("type");
-            String password = (String) requestBodyMap.get("password");
-            String hashURL = (String) requestBodyMap.get("hashURL");
-            Integer userId;
-            GenerateHashPassword generate_hash_password = new GenerateHashPassword();
-            String hashPassword = generate_hash_password.hashPass(password);
-            if (type.equalsIgnoreCase("update")) {
-                UserProfile userProfile = (UserProfile) UserSessionUtil.getLogedInUser();
-                userId = userProfile.getUser().getUserId();
-                forgotPasswordService.updatePassword(userId, hashPassword);
-            } else if (type.equalsIgnoreCase("change")) {
-                ForgotPassword forgotPassword = forgotPasswordService.getByRandomHash(hashURL);
-                forgotPasswordService.updatePassword(forgotPassword.getFkUserId().getUserId(), hashPassword);
-                forgotPasswordService.delete(forgotPassword);
+            Boolean result = emailListService.setEmailList(requestBodyMap, userId);
+            if (result) {
+                transactionResponse.setOperationStatus(ErrorHandlingUtil.dataNoErrorValidation(messageSource.getMessage("success", new String[]{}, Locale.US)));
+            } else {
+                logger.debug("Unable to set email list");
+                transactionResponse.setOperationStatus(ErrorHandlingUtil.dataErrorValidation(messageSource.getMessage("fail", new String[]{}, Locale.US)));
             }
 
-            transactionResponse.setOperationStatus(ErrorHandlingUtil.dataNoErrorValidation(messageSource.getMessage("signup_passwordchangesuccessfully", new String[]{}, Locale.US)));
-
         } catch (Throwable throwable) {
             logger.error(throwable);
             transactionResponse.setOperationStatus(ErrorHandlingUtil.dataErrorValidation(throwable.getMessage()));
         }
         return new ResponseEntity<>(new ContainerResponse(transactionResponse), HttpStatus.ACCEPTED);
     }
+
 }
