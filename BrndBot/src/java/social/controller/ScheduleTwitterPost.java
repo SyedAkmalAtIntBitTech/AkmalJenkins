a57355db2373d86c378509556b1e0f6c0361ea39
ajitpandeyatintbittech <ajit.pandey@intbittech.com>
2016-04-27 15:12:23 +0530

    refactored ScheduleTwitterPost

--- BrndBot/src/java/social/controller/ScheduleTwitterPost.java
+++ BrndBot/src/java/social/controller/ScheduleTwitterPost.java
@@ -6,16 +6,13 @@
 package social.controller;
 
 import com.controller.IConstants;
-import com.controller.SocialPostScheduler;
-import com.divtohtml.StringUtil;
 import com.intbit.AppConstants;
-import com.intbit.marketing.model.TblScheduledEntityList;
-import com.intbit.marketing.model.TblScheduledSocialpostList;
+import com.intbittech.model.ScheduledEntityList;
+import com.intbittech.model.ScheduledSocialpostList;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.concurrent.Callable;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.json.simple.JSONObject;
@@ -34,8 +31,8 @@ public class ScheduleTwitterPost implements Runnable {
     public void run() {
 
         try {
-            List<TblScheduledEntityList> scheduledTwitterPost = getLatestApprovedTwitterPost();
-            for (TblScheduledEntityList currentScheduledTwitterPost : scheduledTwitterPost) {
+            List<ScheduledEntityList> scheduledTwitterPost = getLatestApprovedTwitterPost();
+            for (ScheduledEntityList currentScheduledTwitterPost : scheduledTwitterPost) {
                 if (currentScheduledTwitterPost != null) {
                     //The time zone of the saved date should be extracted.
                     //This time zone should be applied to the current time and then this comparison needs to be made.
@@ -44,27 +41,27 @@ public class ScheduleTwitterPost implements Runnable {
 //                boolean shouldPostNow = true;
 
                     if (shouldPostNow) {
-                        TblScheduledSocialpostList twitterPost = getTwitterPost(currentScheduledTwitterPost);
-                        String jsonString = twitterPost.getMetadata();
+                        ScheduledSocialpostList twitterPost = getTwitterPost(currentScheduledTwitterPost);
+                        String jsonString = twitterPost.getMetaData();
                         JSONObject jsonObject = (JSONObject) new JSONParser().parse(jsonString);
                         String text = jsonObject.get(IConstants.kTwitterTextKey).toString();
                         String url = jsonObject.get(IConstants.kTwitterURLKey).toString();
                         PostToTwitter postToTwitter = new PostToTwitter();
-                        Integer userId = currentScheduledTwitterPost.getUserId();
-                        String twitterAccessToken = postToTwitter.getTwitterAccessToken(userId);
-                        String twitterTokenSecret = postToTwitter.getTwitterAccessTokenSecret(userId);
+                        Integer comapnyId = currentScheduledTwitterPost.getFkCompanyId().getCompanyId();
+                        String twitterAccessToken = postToTwitter.getTwitterAccessToken(comapnyId);
+                        String twitterTokenSecret = postToTwitter.getTwitterAccessTokenSecret(comapnyId);
 
                         String file_image_path = "";
                         if (twitterPost.getImageType().equals("layout")) {
                             file_image_path = AppConstants.LAYOUT_IMAGES_HOME + File.separator + twitterPost.getImageName();
                         } else if (twitterPost.getImageType().equals("gallery")) {
-                            file_image_path = AppConstants.USER_IMAGE_HOME + File.separator + userId + File.separator + twitterPost.getImageName();
+                            file_image_path = AppConstants.USER_IMAGE_HOME + File.separator + comapnyId + File.separator + twitterPost.getImageName();
                         } else if (twitterPost.getImageType().equals("url")) {
                             file_image_path = twitterPost.getImageName();
                         }
 
                         Logger.getLogger(ScheduleTwitterPost.class.getName()).log(Level.SEVERE, text + " Tweet @" + new Date());
-                        String message = PostToTwitter.postStatus(twitterAccessToken, twitterTokenSecret, twitterPost.getImageType(), text, url, file_image_path, userId, null, twitterPost.getImageName());
+                        String message = PostToTwitter.postStatus(twitterAccessToken, twitterTokenSecret, twitterPost.getImageType(), text, url, file_image_path, comapnyId, null, twitterPost.getImageName());
                         if (message.equalsIgnoreCase("success")) {
                             updateStatusScheduledTwitter(currentScheduledTwitterPost);
                         }
@@ -77,25 +74,25 @@ public class ScheduleTwitterPost implements Runnable {
         }
     }
 
-    private void updateStatusScheduledTwitter(TblScheduledEntityList scheduledTwitterPost) throws Throwable {
+    private void updateStatusScheduledTwitter(ScheduledEntityList scheduledTwitterPost) throws Throwable {
         //Call the DAO here
-        logger.log(Level.INFO, "Twitter post:" + scheduledTwitterPost.getScheduleTitle() + "Id:" + scheduledTwitterPost.getId() + " time:" + scheduledTwitterPost.getScheduleTime().toString());
+        logger.log(Level.INFO, "Twitter post:" + scheduledTwitterPost.getScheduleTitle() + "Id:" + scheduledTwitterPost.getScheduledEntityListId() + " time:" + scheduledTwitterPost.getScheduleTime().toString());
         scheduledTwitterPost.setStatus(IConstants.kSocialPostCommpleteStatus);
         SchedulerUtilityMethods.updateScheduledEntityListEntity(scheduledTwitterPost);
     }
 
-    private TblScheduledSocialpostList getTwitterPost(TblScheduledEntityList scheduledTwitterPost) throws Throwable {
+    private ScheduledSocialpostList getTwitterPost(ScheduledEntityList scheduledTwitterPost) throws Throwable {
         //Call the DAO here
-        TblScheduledSocialpostList scheduledSocialpostList = SchedulerUtilityMethods.getSocialPostEntityById(scheduledTwitterPost.getEntityId());
+        ScheduledSocialpostList scheduledSocialpostList = SchedulerUtilityMethods.getSocialPostEntityById(scheduledTwitterPost.getEntityId());
         return scheduledSocialpostList;
     }
 
-    private List<TblScheduledEntityList> getLatestApprovedTwitterPost() throws Throwable {
-        List<TblScheduledEntityList> scheduledEntityList = new ArrayList<TblScheduledEntityList>();
+    private List<ScheduledEntityList> getLatestApprovedTwitterPost() throws Throwable {
+        List<ScheduledEntityList> scheduledEntityList = new ArrayList<ScheduledEntityList>();
         ArrayList<String> entityId = SchedulerUtilityMethods.getLatestApprovedPost(IConstants.kSocialPostapprovedStatus, IConstants.kTwitterKey, IConstants.kUserMarketingProgramOpenStatus);
         if (entityId.size() > 0) {
             for (String currentEntityId : entityId) {
-                TblScheduledEntityList tblScheduledEntityList = SchedulerUtilityMethods.getEntityById(Integer.parseInt(currentEntityId), IConstants.kTwitterKey);
+                ScheduledEntityList tblScheduledEntityList = SchedulerUtilityMethods.getEntityById(Integer.parseInt(currentEntityId), IConstants.kTwitterKey);
                 scheduledEntityList.add(tblScheduledEntityList);
             }
         }
