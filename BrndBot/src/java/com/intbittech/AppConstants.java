b9706ee90ba1ff9ddb9bfa0392b957deec80ab70
SyedIlyasAtIntBitTech <syed.ilyas@intbittech.com>
2016-04-30 20:45:18 +0530

    Removed two template folder and now they will be saved to case EMAIL_BLOCK_TEMPLATE_IMAGE:

--- BrndBot/src/java/com/intbittech/AppConstants.java
+++ BrndBot/src/java/com/intbittech/AppConstants.java
@@ -28,6 +28,5 @@ public class AppConstants {
     public static final String BASE_IMAGE_COMPANY_UPLOAD_PATH = BASE_USERS_IMAGE_UPLOAD_PATH + File.separator + "companies";
     public static final String BASE_ADMIN_GLOBAL_IMAGE_UPLOAD_PATH = BASE_ADMIN_IMAGE_UPLOAD_PATH + File.separator + "globalimages";
     public static final String BASE_ADMIN_EMAIL_TEMPLATE_IMAGE_UPLOAD_PATH = BASE_ADMIN_IMAGE_UPLOAD_PATH + File.separator + "emailtemplates";
-    public static final String BASE_ADMIN_EMAIL_BLOCK_TEMPLATE_IMAGE_UPLOAD_PATH = BASE_ADMIN_IMAGE_UPLOAD_PATH + File.separator + "emailblocktemplates";
     
 }
