e44048e29490a501027acffd22eac6b4e9a86446
AbdulRaqeeb AbdulAzeez <ar@intbittech.com>
2016-05-04 16:53:15 -0400

    Settings page fixes

--- BrndBot/web/static_resources/user/js/settings.js
+++ BrndBot/web/static_resources/user/js/settings.js
@@ -169,104 +169,7 @@ function validate() {
 }
 
 function controllerUserChanges($scope, $http) {
-    $scope.getLooks = function () {
-
-        $http({
-            method: 'GET',
-            url: 'GetLooks?type=selected'
-        }).success(function (data, status, headers, config) {
-            $scope.UserLooks = data;
-            if (data === error) {
-            }
-        }).error(function (data, status, headers, config) {
-            alert(nodataerror);
-        });
-
-        $http({
-            method: 'GET',
-            url: 'GetLooks'
-        }).success(function (data, status, headers, config) {
-            $scope.First = data.first;
-            $scope.Second = data.second;
-            if (data === error) {
-            }
-        }).error(function (data, status, headers, config) {
-            alert(nodataerror);
-        });
-    };
-
-    $scope.updateLooks = function () {
-        var look_id = $("#lookid").val();
-        if (look_id == "") {
-            alert(lockerror);
-        } else {
-            $http({
-                method: 'POST',
-                url: 'SetLookID?type=update&LookID=' + look_id
-
-            }).success(function (data, status, headers, config) {
-                $scope.status = data;
-                if (data === "true") {
-                    alert(detailssaveds);
-                    $scope.getLooks();
-                } else
-                if (data === error) {
-                }
-            }).error(function (data, status, headers, config) {
-                alert(nodataerror);
-            });
-        }
-    };
-
-    $scope.getBrands = function () {
-
-        $http({
-            method: 'GET',
-            url: 'GetBrandPersonality?type=selected'
-        }).success(function (data, status, headers, config) {
-            $scope.UserBrand = data;
-            if (data === error) {
-            }
-        }).error(function (data, status, headers, config) {
-            alert(nodataerror);
-        });
-
-        $http({
-            method: 'GET',
-            url: 'GetBrandPersonality?type=all'
-        }).success(function (data, status, headers, config) {
-            $scope.First = data.first;
-            if (data === error) {
-            }
-        }).error(function (data, status, headers, config) {
-            alert(nodataerror);
-        });
-    };
-
-    $scope.updateBrands = function () {
-
-        var brandID = $("#hiddenform").val();
-        if (brandID == "") {
-            alert(personalityerror);
-        } else {
-            $http({
-                method: 'POST',
-                url: 'SubbrandPersonality?brndID=' + brandID + '&type=update'
-
-            }).success(function (data, status, headers, config) {
-                $scope.status = data;
-                if (data === "true") {
-                    alert(detailssaved);
-                    $scope.getBrands();
-                } else
-                if (data === error) {
-                }
-            }).error(function (data, status, headers, config) {
-                alert(nodataerror);
-            });
-        }
-    };
-
+    
     $scope.changePassword = function () {
         var password = "";
         var confirmpassword = "";
@@ -321,25 +224,12 @@ function controllerUserChanges($scope, $http) {
             alert(nodataerror);
         });
 
-        $http({
-            method: 'GET',
-            url: 'GetColorPalettes'
-        }).success(function (data, status, headers, config) {
-
-            $scope.themes = data;
-
-            if (data === error) {
-            }
-        }).error(function (data, status, headers, config) {
-            alert(nodataerror);
-        });
-
     };
 
     $scope.getLogoColors = function () {
         $http({
             method: 'GET',
-            url: getHost() +'/onboarding/getColorsForLogo'
+            url: getHost() +'onboarding/getColorsForLogo'
         }).success(function (data, status, headers, config) {
             $scope.color =data.d.details;
             if (data === error) {
