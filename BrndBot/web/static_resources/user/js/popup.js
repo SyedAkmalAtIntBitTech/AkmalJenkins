acd3fdf129172f18942bcdd9fe36ddfd60a4b2c5
SandeepKumarAtIntBitTech <sandeep.kumar@intbittech.com>
2016-05-09 11:27:48 +0530

    removed gif image from socialsequence to continue flow

--- BrndBot/web/static_resources/user/js/popup.js
+++ BrndBot/web/static_resources/user/js/popup.js
@@ -1376,7 +1376,6 @@ angular.module("socialMedia", [])
              });
     }; 
     $scope.checkForCode = function(){
-        showOverlay();
         var code=getUrlParameter("code");
            if(typeof code !=="undefined"){
                 $http({
