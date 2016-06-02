eb7dc3c6753333465ffff28ec4e89d668aa8b79c
SandeepKumarAtIntBitTech <sandeep.kumar@intbittech.com>
2016-06-02 13:18:02 +0530

    Added signup page and user folder

---
+++ BrndBot/web/static_resources/js/header.js
@@ -0,0 +1,35 @@
+/* 
+ * Copyright 2015 Intbit Technologies. This software and documentation contains
+ * confidential and proprietary information that is owned by Intbit
+ * Technologies. Unauthorized use and distribution are strictly prohibited.
+ */
+var pageHeaderHtml = '<link rel="stylesheet" type="text/css" href="css/General-Utility.css">';
+pageHeaderHtml+='<link rel="stylesheet" type="text/css" href="css/Navigation.css">';
+pageHeaderHtml+='<link rel="stylesheet" type="text/css" href="css/Pane.css">';
+pageHeaderHtml+='<link rel="stylesheet" type="text/css" href="css/Page-Structure.css">';
+pageHeaderHtml+='<link rel="stylesheet" type="text/css" href="css/Dashboard.css">';
+pageHeaderHtml+='<link rel="stylesheet" type="text/css" href="css/Email.css">';
+pageHeaderHtml+='<link rel="stylesheet" type="text/css" href="css/normalize.css">';
+pageHeaderHtml+='<link rel="stylesheet" type="text/css" href="css/Form-Input.css">';
+pageHeaderHtml+='<link rel="stylesheet" type="text/css" href="css/overlay-drawer.css">';
+pageHeaderHtml+='<link rel="stylesheet" type="text/css" href="css/sequence-Pages.css">';
+pageHeaderHtml+='<link rel="stylesheet" type="text/css" href="css/slats.css">';
+pageHeaderHtml+='<link rel="stylesheet" type="text/css" href="css/onboarding.css">';
+pageHeaderHtml+='<link rel="stylesheet" type="text/css" href="css/login-onboarding.css"></link>';
+pageHeaderHtml+='<link rel="stylesheet" type="text/css" href="css/admin.css"></link>';
+pageHeaderHtml+='<script src="js/jquery-1.11.3.min.js" type="text/javascript"></script>'; 
+pageHeaderHtml+='<script src="js/alertmessage.js" type="text/javascript"></script>';
+pageHeaderHtml+='<script src="js/onboarding.js" type="text/javascript"></script>'; 
+pageHeaderHtml+='<script src="js/configurations.js"></script>';   
+pageHeaderHtml+='<script src="js/userbase64conversion.js"></script>';   
+pageHeaderHtml+='<link href="css/colpick.css" rel="stylesheet" type="text/css"/>';
+pageHeaderHtml+='<script src="js/colpick.js" type="text/javascript"></script>';
+pageHeaderHtml+='<script src="js/form.js" type="text/javascript"></script>';
+pageHeaderHtml+='<link rel="shortcut icon" href="images/favicon.png"/>';
+
+document.write(pageHeaderHtml);
+
+
+
+
+       
