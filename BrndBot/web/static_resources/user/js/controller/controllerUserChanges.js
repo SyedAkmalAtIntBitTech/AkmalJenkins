8ea2aea8857d73acdea899abe5db7254609d5f58
satyajitroyatintbittech <SatyajitRoyAtIntBitTech@github.com>
2016-06-10 11:41:39 +0530

    modified change password code confirm password code from jquery to angularjs

--- BrndBot/web/static_resources/user/js/controller/controllerUserChanges.js
+++ BrndBot/web/static_resources/user/js/controller/controllerUserChanges.js
@@ -1,6 +1,19 @@
 settingFlowApp.controller("controllerUserChanges", ['$scope', '$window', '$location', 'signupFactory', 'settingsFactory', 'assetsFactory', 'onboardingFactory', function ($scope, $window, $location, signupFactory, settingsFactory, assetsFactory, onboardingFactory) {
-        $scope.changePassword = function (password, confirmpassword) {
-            var password_object = {"password": password, "confirmpassword": confirmpassword, "type": "update"};
+ 
+  $scope.inputType = 'password';
+  
+  // Hide & show password function
+  $scope.hideShowPassword = function(){
+    if ($scope.inputType == 'password')
+      $scope.inputType = 'text';
+    else
+      $scope.inputType = 'password';
+  };
+        $scope.changePassword = function (password,confirmPassword) {
+            alert(password);
+            alert(confirmPassword);
+            
+            var password_object = {"password": password, "confirmpassword": confirmPassword, "type": "update"};
             signupFactory.resetPasswordPost(password_object).then(function (data) {
                 debugAlert(JSON.stringify(data));
                 $scope.status = data;
@@ -8,7 +21,9 @@ settingFlowApp.controller("controllerUserChanges", ['$scope', '$window', '$locat
         };
 
         $scope.showColors = function () {
+           
             settingsFactory.getColorsURLGet().then(function (data) {
+                
                 $scope.user_preferences_colors = data.d.details;
             });
 
@@ -23,12 +38,17 @@ settingFlowApp.controller("controllerUserChanges", ['$scope', '$window', '$locat
             });
         };
         
-        $scope.createUserPreferences = function ()
+        
+       $scope.createUserPreferences = function (themecolor1,themecolor2,themecolor3,themecolor4)
         {
-            color1 = "rgb(255, 0, 255)";
-            color2 = "rgb(255, 0, 255)";
-            color3 = "rgb(255, 0, 255)";
-            color4 = "rgb(255, 0, 255)";
+            alert(themecolor1);
+            alert(themecolor2);
+            alert(themecolor3);
+            alert(themecolor4);
+//            color1 = "rgb(255, 0, 255)";
+//            color2 = "rgb(255, 0, 255)";
+//            color3 = "rgb(255, 0, 255)";
+//            color4 = "rgb(255, 0, 255)";
             settingsFactory.setColorsPost(color1,color2,color3,color4).then(function (data) {
                 $scope.status = data;
                 alert(data.d.operationStatus.messages[0]);
