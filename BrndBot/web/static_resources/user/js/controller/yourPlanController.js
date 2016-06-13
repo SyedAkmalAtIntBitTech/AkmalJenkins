00e502f26dca11fbd5fae465e1344a1c32f8b036
Rasim Parvez <rasim.parvez@intbittech.com>
2016-06-13 11:13:29 +0530

    files modiefed

--- BrndBot/web/static_resources/user/js/controller/yourPlanController.js
+++ BrndBot/web/static_resources/user/js/controller/yourPlanController.js
@@ -1,15 +1,17 @@
 
 yourPlanFlowApp.controller("yourPlanController", ['$scope', '$location', 'yourPlanFactory', function ($scope, $location, yourPlanFactory) {
   $scope.getCampaigns = function(){
-        
+       
   yourPlanFactory.scheduledEntitiesGet("2016-05-03","2016-06-10").then(function (data) {
-     
-    
    var yourplanData=JSON.parse(JSON.stringify(data.d.details));
-  $scope.entitySet= JSON.stringify(JSON.parse(yourplanData).entitydata[9]);
-  $scope.nodata = eval(JSON.stringify(JSON.parse(yourplanData).noactionsmessage));
+    var entityDetails =  JSON.stringify(JSON.parse(yourplanData).entitydata);
+     $scope.entitySet = entityDetails;
+     var noData = JSON.stringify(JSON.parse(yourplanData).noactionsmessage)
+
+//   $scope.nodata = eval(noData);
+//   alert(noData);
   
-  alert(JSON.stringify(JSON.parse(yourplanData).entitydata[9].dataArray));
+  alert($scope.entitySet);
      
   });
   }
