f46f93e9375a579f7eb65cab6925f6286ddf6695
satyajitroyatintbittech <SatyajitRoyAtIntBitTech@github.com>
2016-06-16 11:30:35 +0530

    Display logo image

--- BrndBot/web/static_resources/user/js/controller/controllerUserChanges.js
+++ BrndBot/web/static_resources/user/js/controller/controllerUserChanges.js
@@ -140,5 +140,21 @@ settingFlowApp.controller("controllerUserChanges", ['$scope', '$window', '$locat
             alert();
             
         };
+        
+        
+        
+            $scope.stepsModel = [];
+
+    $scope.imageUpload = function(element){
+        var reader = new FileReader();
+        reader.onload = $scope.imageIsLoaded;
+        reader.readAsDataURL(element.files[0]);
+    };
+
+    $scope.imageIsLoaded = function(e){
+        $scope.$apply(function() {
+            $scope.stepsModel.push(e.target.result);
+        });
+    };
 
     }]);
