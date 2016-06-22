bfb07df0261d21606eb893ba3b749219f9ea5053
satyajitroyatintbittech <SatyajitRoyAtIntBitTech@github.com>
2016-06-22 10:06:11 +0530

    Removed alerts

--- BrndBot/web/static_resources/user/js/controller/imageGalleryController.js
+++ BrndBot/web/static_resources/user/js/controller/imageGalleryController.js
@@ -7,51 +7,47 @@
 imagesFlowApp.controller("imageGalleryController", ['$scope', '$window', '$http', '$location', 'imageFactory','companyImagesFactory',function ($scope, $window, $http, $location, imageFactory,companyImagesFactory) {
    
 
-        $scope.uploadLogo = function (myFile1) {
-            
+        $scope.uploadLogo = function (myFile1) {   
          var file = $("#filesToUpload").val();         
         var file = myFile1;
             imageFactory.saveImagePost(file).then(function (data){
                 alert("Image Uploaded successfully");
-        });
-    };
+                $scope.hidePopup=false;
+                $scope.getAllCompanyImages();
+            });
+        };
         
         $scope.getAllCompanyImages = function ()
         {
             companyImagesFactory.companyImagesGet().then(function (data){
                 $scope.datalists = data.d.details;
-                
-               
             });
         };
         
         $scope.deleteImage = function (companyImageId)
         {
             companyImagesFactory.deleteCompanyImagesPost(companyImageId).then(function (data){
-                alert(JSON.stringify(data));
+                alert("Image deleted successfully");
                 $scope.datalists = data.d.details;
                 $scope.getAllCompanyImages();
-                
-               
             });
         };
         
-
-        
         $scope.showImgageUploadPopup = function()
         {
-        $scope.hidePopup=true;
+            $scope.hidePopup=true;
         };
     
-    $scope.closeImgageUploadPopup = function()
-    {
-        $scope.hidePopup=false;
-        $scope.getAllCompanyImages();
-    };
+        $scope.closeImgageUploadPopup = function()
+        {
+            $scope.hidePopup=false;
+            $scope.getAllCompanyImages();
+        };
     
-      $scope.showImage = function(){
-       $scope.hidePopup=false;
+        $scope.showImage = function()
+        {
+            $scope.hidePopup=false;
         };
 
-              }]);
+}]);
 
