0f8780a3ebb100e99c76601328912db03d6b12d2
SyedIlyasAtIntBitTech <syed.ilyas@intbittech.com>
2016-06-24 15:09:27 +0530

    Added few more changes

--- BrndBot/web/static_resources/js/factory/companyMarketingFactory.js
+++ BrndBot/web/static_resources/js/factory/companyMarketingFactory.js
@@ -69,10 +69,10 @@ factoryApp.factory('companyMarketingProgramFactory', function ($q, authenticated
         });
         return deffered.promise;
     };
-    companyMarketingProgramFactoryObject.approveStatusPost = function () {
+    companyMarketingProgramFactoryObject.approveStatusPost = function (data) {
         var deffered = $q.defer();
         var url = configurationService.approveStatusURL();
-        authenticatedServiceFactory.makeCall("POST", url, "").then(function (data) {
+        authenticatedServiceFactory.makeCall("POST", url, data,"").then(function (data) {
             deffered.resolve(data);
         });
         return deffered.promise;
