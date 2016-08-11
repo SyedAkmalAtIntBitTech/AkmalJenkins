054a87b7432f1f7714b4e2b88afc31d5654d38ef
SyedIlyasAtIntBitTech <syed.ilyas@intbittech.com>
2016-08-11 13:12:14 +0530

    Added appSessionFactory

---
+++ BrndBot/web/static_resources/js/factory/appSessionFactory.js
@@ -0,0 +1,59 @@
+//************************ @author Syed Ilyas @ Intbit *************************
+
+factoryApp.factory('appSessionFactory', function ($q) {
+    var AppSessionFactoryObject = {};
+    
+    //namespace details
+    var emailObjectName = "email";
+    var email = { };
+    AppSessionFactoryObject.setEmail = function (key, value) {
+        var deffered = $q.defer();
+        var data = false;
+        if(key && value)
+        {
+//            var keyString = email+"."+key;
+            email[key] = value
+            localStorage.setItem(emailObjectName,JSON.stringify(email));
+            data = true;
+        }
+        deffered.resolve(data);
+        return deffered.promise;
+    };
+    
+    AppSessionFactoryObject.getEmail = function (key) {
+        var deffered = $q.defer();
+        var requestedValue = "";
+        if(key)
+        {
+            var emailLocalObject = JSON.parse(localStorage.getItem(emailObjectName));
+            if(emailLocalObject){
+                var value = emailLocalObject[key];
+                if(value)
+                    requestedValue = value;
+            }
+        }
+        deffered.resolve(requestedValue);
+        return deffered.promise;
+    };
+    
+    AppSessionFactoryObject.clearAllEmail = function () {
+        var deffered = $q.defer();
+        var data = true;
+        localStorage.removeItem(emailObjectName);
+        deffered.resolve(data);
+        return deffered.promise;
+    };
+    AppSessionFactoryObject.clearKeyEmail = function (key) {
+        var deffered = $q.defer();
+        var data = true;
+        var emailLocalObject = JSON.parse(localStorage.getItem(emailObjectName));
+        if(emailLocalObject){
+            if(emailLocalObject[key])
+                emailLocalObject[key] = "";
+            localStorage.setItem(emailLocalObject);
+        }
+        deffered.resolve(data);
+        return deffered.promise;
+    };
+    return AppSessionFactoryObject;
+});
