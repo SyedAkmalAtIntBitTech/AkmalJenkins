a3be987cb5813934df3d6f58729d08a350539161
AbdulRaqeeb AbdulAzeez <ar@intbittech.com>
2016-08-13 13:41:30 -0400

    changed it back to 8080

--- BrndBot/web/static_resources/js/configurations.js
+++ BrndBot/web/static_resources/js/configurations.js
@@ -7,7 +7,7 @@
 //var global_host_address = "http://development.brndbot.com/BrndBot/";
 //var global_host_address = "http://development2.brndbot.com/BrndBot/";
 //var global_host_address = "http://dev1.intbittech.com/BrndBot/";
-var global_host_address = "http://localhost:9090/BrndBot/";
+var global_host_address = "http://localhost:8080/BrndBot/";
 var error = "system failure error";  
 var FroalaLicenseKey ="snJ-7c1krD-13fD1wzF-7==";
 var defaultFroalaColors= ["61BD6D","clolor1",
