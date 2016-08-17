777dc18d1ee9b0346dd9b9e884fd64e5e5574f73
Ajit <ajit.pandey@intbittech.com>
2016-08-17 19:33:19 +0530

    added activity service

---
+++ BrndBot/src/java/com/intbittech/services/ActivityLogService.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright 2015 Intbit Technologies. This software and documentation contains
+ * confidential and proprietary information that is owned by Intbit
+ * Technologies. Unauthorized use and distribution are strictly prohibited.
+ */
+package com.intbittech.services;
+
+/**
+ * <code>{@link ActivityLogService}</code> is service layer interface for
+ * communicating between Controller and DAO classes
+ *
+ * @author Ajit
+ */
+public interface ActivityLogService {
+    
+}
