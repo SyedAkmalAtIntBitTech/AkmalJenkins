f356edd1f1792b6f40536c259699e9c99e8ce4ed
Ajit <ajit.pandey@intbittech.com>
2016-08-17 20:19:14 +0530

    remove unused code

--- BrndBot/src/java/com/intbittech/model/Users.java
+++ BrndBot/src/java/com/intbittech/model/Users.java
@@ -7,9 +7,7 @@ package com.intbittech.model;
 
 import java.io.Serializable;
 import java.util.Date;
-import java.util.Set;
 import javax.persistence.Basic;
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -17,11 +15,9 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
-import javax.xml.bind.annotation.XmlTransient;
 
 /**
  *
