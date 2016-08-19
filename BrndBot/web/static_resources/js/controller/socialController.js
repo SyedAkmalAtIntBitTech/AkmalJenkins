c435704be337628b4417148500ff3e787c308f26
SandeepKumarAtIntBitTech <sandeep.kumar@intbittech.com>
2016-08-19 18:50:59 +0530

    image change issue and close button issue of emailautomation preview popup fixed

--- BrndBot/web/static_resources/js/controller/socialController.js
+++ BrndBot/web/static_resources/js/controller/socialController.js
@@ -1046,7 +1046,6 @@ socialFlowApp.controller("socialController", ['$scope', '$rootScope', '$location
             $scope.$apply(function () {
                 $scope.imageModal = [];
                 $scope.imageModal.push(e.target.result);
-                $scope.showImageUploaded = false;
             });
             var showImage = document.getElementById('upload');
             angular.element(showImage).triggerHandler('click');
@@ -1055,8 +1054,6 @@ socialFlowApp.controller("socialController", ['$scope', '$rootScope', '$location
             var file = myFile;
             imageFactory.saveImagePost(file).then(function (data) {
                 growl("Image Uploaded successfully");
-                $scope.hidePopup = false;
-                $("#fade").hide();
                 $scope.getUserImages();
             });
         };
