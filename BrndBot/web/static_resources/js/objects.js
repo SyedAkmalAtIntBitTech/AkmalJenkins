b1d802a4d4afa1d479fa8a47cb9c34c1a4555a3d
SyedIlyasAtIntBitTech <syed.ilyas@intbittech.com>
2016-08-24 13:27:26 +0530

    init refactor email object - did refactor for draft

---
+++ BrndBot/web/static_resources/js/objects.js
@@ -0,0 +1,27 @@
+/* 
+ * Copyright 2015 Intbit Technologies. This software and documentation contains
+ * confidential and proprietary information that is owned by Intbit
+ * Technologies. Unauthorized use and distribution are strictly prohibited.
+ */
+
+var kGlobalEmailObject = { categoryId : "",
+    subCategoryId : "",
+    lookupId : "",
+    mindbody : "",
+    mindbodyId : "",
+    draftId : "",
+    emailSubject : "",
+    entityScheduleId : "",
+    email_Schedule_Id : "",
+    to_email_addresses : "",
+    body : "",
+    email_list_name : "",
+    from_name : "",
+    reply_to_email_address : "",
+    from_address : "",
+    preheader : "",
+    emailDraftData : "",
+    htmlBody : ""
+    };
+
+
