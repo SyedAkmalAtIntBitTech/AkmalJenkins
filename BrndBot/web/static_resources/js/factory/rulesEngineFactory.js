7a982dfd618fa97101cff8ff22230fda2e2bb1ed
SyedIlyasAtIntBitTech <syed.ilyas@intbittech.com>
2016-08-29 16:12:20 +0530

    Completed rulesEngineFactory

--- BrndBot/web/static_resources/js/factory/rulesEngineFactory.js
+++ BrndBot/web/static_resources/js/factory/rulesEngineFactory.js
@@ -1,14 +1,22 @@
 //************************ @author Syed Ilyas @ Intbit *************************
 
-factoryApp.factory('rulesEngineFactory', function ($q) {
+factoryApp.factory('rulesEngineFactory', ['$q','appSessionFactory', function ($q, appSessionFactory) {
     var RulesEngineFactoryObject = {};
+    var creatorRoleConstant = "Creator";
+    var managerRoleConstant = "Manager";
+    
     RulesEngineFactoryObject.isActionEligibleForUser = function() {
         var deffered = $q.defer();
-        var data = false;
-        
-        deffered.resolve(data);
+        var data = true;
+        appSessionFactory.getCompany().then(function (kGlobalCompanyObject){
+            alert(kGlobalCompanyObject.roleName);
+            if(kGlobalCompanyObject.roleName==creatorRoleConstant)
+                data = false;
+            deffered.resolve(data);
+        });
         return deffered.promise;
     };
-});
+    return RulesEngineFactoryObject;
+}]);
 
 
