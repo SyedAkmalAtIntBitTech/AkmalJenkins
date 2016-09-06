5d5d4194f5ed6d34cffd47348ae6e5c791be0582
Ajit <ajit.pandey@intbittech.com>
2016-09-06 14:42:50 +0530

    added saveSendGridSubUserDetails

--- BrndBot/src/java/com/intbittech/controller/SendGridSubUserDetailsController.java
+++ BrndBot/src/java/com/intbittech/controller/SendGridSubUserDetailsController.java
@@ -7,19 +7,24 @@ package com.intbittech.controller;
 
 import com.intbittech.model.Company;
 import com.intbittech.model.SendGridSubUserDetails;
+import com.intbittech.model.UserCompanyIds;
 import com.intbittech.model.Users;
+import com.intbittech.modelmappers.SendGridSubUserDetailsRequest;
 import com.intbittech.responsemappers.ContainerResponse;
 import com.intbittech.responsemappers.TransactionResponse;
 import com.intbittech.services.SendGridSubUserDetailsService;
 import com.intbittech.utility.ErrorHandlingUtil;
+import com.intbittech.utility.Utility;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  *
@@ -31,19 +36,20 @@ public class SendGridSubUserDetailsController {
     private final static Logger logger = Logger.getLogger(SendGridSubUserDetailsController.class);
    @Autowired
    private SendGridSubUserDetailsService sendGridSubUserDetailsService;
-    @RequestMapping(value = "saveSendGridSubUserDetails", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<ContainerResponse> saveSendGridSubUserDetails() {
+    @RequestMapping(value = "saveSendGridSubUserDetails", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<ContainerResponse> saveSendGridSubUserDetails(@RequestBody SendGridSubUserDetailsRequest sendGridSubUserDetailsRequest) {
         TransactionResponse transactionResponse = new TransactionResponse();
         try {
             SendGridSubUserDetails sendGridSubUserDetails = new SendGridSubUserDetails();
-            sendGridSubUserDetails.setSendGridUserId("demo");
+            sendGridSubUserDetails.setIps(sendGridSubUserDetailsRequest.getIps());
+            sendGridSubUserDetails.setSendGridUserId(sendGridSubUserDetailsRequest.getSendGridUserId());
+            Users users = new Users();
+            users.setUserId(sendGridSubUserDetailsRequest.getUserId());
+            sendGridSubUserDetails.setFkUserId(users);
             Company company = new Company();
-            company.setCompanyId(2);
-           sendGridSubUserDetails.setFkCompanyId(company);
-           Users user = new Users();
-           user.setUserId(1);
-           sendGridSubUserDetails.setFkUserId(user);
-           sendGridSubUserDetailsService.save(sendGridSubUserDetails);
+            company.setCompanyId(sendGridSubUserDetailsRequest.getCompanyId());
+            sendGridSubUserDetails.setFkCompanyId(company);
+            sendGridSubUserDetailsService.save(sendGridSubUserDetails);
           transactionResponse.setOperationStatus(ErrorHandlingUtil.dataNoErrorValidation("Send grid sub userDetails created successfully"));
         } catch (Throwable ex) {
             logger.error(ex);
