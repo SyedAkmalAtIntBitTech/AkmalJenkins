fc488c84ed1fb8ccfa68cfd3059ed5f8759ef815
Syed Muzamil <syed.muzamil@intbittech.com>
2016-09-07 19:39:32 +0530

    added the assignTo user in the addAction popup and written the controller methods

--- BrndBot/src/java/com/intbittech/model/UserRole.java
+++ BrndBot/src/java/com/intbittech/model/UserRole.java
@@ -6,15 +6,12 @@
 package com.intbittech.model;
 
 import java.io.Serializable;
-import java.util.Set;
 import javax.persistence.Basic;
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 
@@ -25,9 +22,6 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "user_role")
 public class UserRole implements Serializable {
-
-    @OneToMany(cascade = CascadeType.ALL, mappedBy = "fkUserRoleId")
-    private Set<Users> usersSet;
 
     private static final long serialVersionUID = 1L;
     @Id
