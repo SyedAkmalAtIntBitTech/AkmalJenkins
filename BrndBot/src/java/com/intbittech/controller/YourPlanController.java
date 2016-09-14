6a8c31de85fee86f24a9ee8b5c17e459a039f32e
Syed Muzamil <syed.muzamil@intbittech.com>
2016-09-14 17:31:06 +0530

    removed the comment

--- BrndBot/src/java/com/intbittech/controller/YourPlanController.java
+++ BrndBot/src/java/com/intbittech/controller/YourPlanController.java
@@ -95,7 +95,6 @@ public class YourPlanController {
             toDate = LocalDate.parse(request.getParameter("to"));
             Integer companyId= Integer.parseInt(request.getParameter("companyId"));
             JSONObject scheduledEntities = 
-                     //ScheduleDAO.getScheduledEntities(companyId, fromDate, toDate);
                      ScheduleDAO.getScheduledEntitiesWithUser(companyId, fromDate, toDate);
             messageList.add(AppConstants.GSON.toJson(scheduledEntities));
             genericResponse.setDetails(messageList);
