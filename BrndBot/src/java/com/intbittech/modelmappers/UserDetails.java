e17fcfe1acfa0e751a4fe5b1d53c6fbc61b37700
SyedIlyasAtIntBitTech <syed.ilyas@intbittech.com>
2016-10-25 11:51:05 +0530

    Removed companyId and userId which were redunant

--- BrndBot/src/java/com/intbittech/modelmappers/UserDetails.java
+++ BrndBot/src/java/com/intbittech/modelmappers/UserDetails.java
@@ -15,11 +15,9 @@ import java.util.Date;
  */
 public class UserDetails extends UserCompanyIds implements Serializable {
     
-    private Integer userId;
     private String userName;
     private String userPassword;
     private Date createdDate;
-    private Integer companyId;
     private String firstName;
     private String lastName;
     private String invitationCode;
@@ -57,14 +55,6 @@ public class UserDetails extends UserCompanyIds implements Serializable {
         this.lastName = lastName;
     }
     
-    public Integer getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Integer userId) {
-        this.userId = userId;
-    }
-
     public String getUserName() {
         return userName;
     }
@@ -89,14 +79,4 @@ public class UserDetails extends UserCompanyIds implements Serializable {
         this.createdDate = createdDate;
     }
 
-    public Integer getCompanyId() {
-        return companyId;
-    }
-
-    public void setCompanyId(Integer companyId) {
-        this.companyId = companyId;
-    }
-    
-    
-    
 }
