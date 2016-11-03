4cf3a68e57907ef30c2ec3e1484662dc49e341fb
syed muzamil <syed.muzamil@intbittech.com>
2016-11-03 18:45:28 +0530

    removed alert

--- BrndBot/web/static_resources/js/controller/onboardingController.js
+++ BrndBot/web/static_resources/js/controller/onboardingController.js
@@ -228,7 +228,6 @@ brndBotSignupApp.controller("onboardingController", ['$scope', '$location', 'sub
             onboardingFactory.getLoggedInUserId().then(function (data) {
                 appSessionFactory.getCompany().then(function (kGlobalCompanyObject) {
                     kGlobalCompanyObject.userId = data.d.details[0];
-                    alert(data.d.details);
                     appSessionFactory.setCompany(kGlobalCompanyObject).then(function (data1) {
                         if (data1) {
                             appSessionFactory.getCompany().then(function (kGlobalCompanyObject) {
